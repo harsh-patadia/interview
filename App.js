876be8a7b04e2b915cec195dab5e28f0b25f7736
harsh-patadia <harshpatadia.88@gmail.com>
2024-11-06 14:43:50 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const data = [
+  { name: "Alice", age: 25, address: "123 St" },
+  { name: "Bob", age: 30, address: "456 St" },
+  { name: "Charlie", age: 25, address: "789 St" },
+  { name: "David", age: 30, address: "101 St" }
+];
+
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header:{
+    flex:1,
+    backgroundColor:'pink'
   },
+  container: {
+    flex: 4,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  footer:{
+    flex:1,
+    backgroundColor:'grey'
+  }
 });
